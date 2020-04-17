4d357e280da7b920ae8ce22913d9bbbdb785b44a
Rugved <rugved.bongale@somaiya.edu>
2020-04-17 12:59:07 +0530

    Added links

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -26,12 +26,12 @@ class About extends Component {
 
   <div className="column">
     <div className="card">
-      <img src="https://scontent.fbom10-1.fna.fbcdn.net/v/t31.0-8/p720x720/10479946_1503231103253462_829683252295770183_o.jpg?_nc_cat=101&_nc_sid=174925&_nc_ohc=f_VL8sI7UxAAX80WDT7&_nc_ht=scontent.fbom10-1.fna&_nc_tp=6&oh=8b8b358a2f12d993aad42f777aab0bcd&oe=5EBB10E3" alt="Jane" style={{width:"100%",height:"200px"}}/>
+      <img src="https://assets.leetcode.com/users/xninjacoder/avatar_1550144758.png" alt="Jane" style={{width:"100%",height:"200px"}}/>
       <div className="container">
         <h3>Tushar Bapecha</h3>
         <p className="title">Developer</p>
         
-        <p>tushar.bapecha@somaiya.edu</p>
+        {/* <p>tushar.bapecha@somaiya.edu</p> */}
         <p><button 
         onClick={(e) => {
           e.preventDefault();
@@ -44,12 +44,12 @@ class About extends Component {
   
   <div className="column">
     <div className="card">
-      <img src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg" alt="Jane" style={{width:"100%",height:"200px"}}/>
+      <img src="https://assets.leetcode.com/users/xninjacoder/avatar_1550144758.png" alt="Jane" style={{width:"100%",height:"200px"}}/>
       <div className="container">
         <h3>Rugved Bongale</h3>
         <p className="title">Developer</p>
         
-        <p>rugved.bongale@somaiya.edu</p>
+        {/* <p>rugved.bongale@somaiya.edu</p> */}
         <p><button
         onClick={(e) => {
           e.preventDefault();
@@ -62,12 +62,12 @@ class About extends Component {
 
   <div className="column">
     <div className="card">
-      <img src="https://scontent.fbom10-1.fna.fbcdn.net/v/t31.0-8/s960x960/1556353_620488651343524_232078041_o.jpg?_nc_cat=106&_nc_sid=174925&_nc_ohc=7Co8eCWwuY0AX-BKIea&_nc_ht=scontent.fbom10-1.fna&_nc_tp=7&oh=468ea57a647ffd8b76d9d367bb31a2f9&oe=5EBBC45E" alt="Jane" style={{width:"100%",height:"200px"}}/>
+      <img src="https://assets.leetcode.com/users/xninjacoder/avatar_1550144758.png" alt="Jane" style={{width:"100%",height:"200px"}}/>
       <div className="container">
         <h3>Talha Chafekar</h3>
         <p className="title">Developer</p>
         
-        <p>talha.c@somaiya.edu</p>
+        {/* <p>talha.c@somaiya.edu</p> */}
         <p><button
         onClick={(e) => {
           e.preventDefault();
@@ -80,12 +80,12 @@ class About extends Component {
 
   <div className="column">
     <div className="card">
-      <img src="https://scontent.fbom10-1.fna.fbcdn.net/v/t31.0-8/s960x960/1074000_1512999872260699_1019771125_o.jpg?_nc_cat=106&_nc_sid=174925&_nc_ohc=GTTohuqGlT8AX9rDHCV&_nc_ht=scontent.fbom10-1.fna&_nc_tp=7&oh=701fb9d193ef2ff04c294eac77806cdd&oe=5EBDDF5C" alt="Jane" style={{width:"100%",height:"200px"}}/>
+      <img src="https://assets.leetcode.com/users/xninjacoder/avatar_1550144758.png" alt="Jane" style={{width:"100%",height:"200px"}}/>
       <div className="container">
         <h3>Mayank C.</h3>
         <p className="title">Developer</p>
         
-        <p>mayank.chowdhary@somaiya.com</p>
+        {/* <p>mayank.chowdhary@somaiya.com</p> */}
         <p><button 
         onClick={(e) => {
           e.preventDefault();
