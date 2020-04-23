cb280d1122fbc2a44eb42199af803b49f65946e0
Rugved <51147262+RugvedB@users.noreply.github.com>
2020-04-23 23:09:30 +0530

    Made Navbar responsive

--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -9,32 +9,34 @@ class NavbarComponent extends React.Component {
 
     return (
       <>
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="/home">
-              <img
-                src={newslogo}
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
-              />
-            </Navbar.Brand>
-            <Navbar.Brand href="/home">DailyNews</Navbar.Brand>
-            
-            <Nav className="mr-auto">
-              <Nav.Link href="/register">Register</Nav.Link>
-              <Nav.Link href="/login">Login</Nav.Link>
-              <Nav.Link href="/home">Home</Nav.Link> 
-            </Nav>
+        
 
-            <Nav className="ml-auto">
-              <Nav.Link href="/OptionPage">Subscripition</Nav.Link>
-              <Nav.Link href="/bookmark">Bookmark</Nav.Link>
-              <Nav.Link href="/About">About Us</Nav.Link>
-              <Nav.Link className="border btn-outline-info" href="/Logout">Logout</Nav.Link>
-            </Nav>
-            
-        </Navbar>
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="/home">
+    <img
+      src={newslogo}
+      width="30"
+      height="30"
+      className="d-inline-block align-top"
+      alt="React Bootstrap logo"
+    />
+  </Navbar.Brand>
+  <Navbar.Brand href="/home">DailyNews</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/register">Register</Nav.Link>
+      <Nav.Link href="/login">Login</Nav.Link>
+      <Nav.Link href="/home">Home</Nav.Link> 
+    </Nav>
+    <Nav>
+      <Nav.Link href="/OptionPage">Subscripition</Nav.Link>
+      <Nav.Link href="/bookmark">Bookmark</Nav.Link>
+      <Nav.Link href="/About">About Us</Nav.Link>
+      <Nav.Link className="border btn-outline-info" href="/Logout">Logout</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
 
 
       </>
